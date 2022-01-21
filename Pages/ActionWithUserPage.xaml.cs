8d34e8a96479f471798d3c8d1c919116239b08f6
Ульяна Сайфутдинована <ulyanasayf@gmail.com>
2022-01-21 15:21:15 +0500

    13223

---
+++ Pages/ActionWithUserPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DefaultWPF.Resources;
+
+namespace DefaultWPF.Pages
+{
+    public partial class ActionWithUserPage : Page
+    {
+        Users currentUser;
+        bool isCreated = true;
+        public ActionWithUserPage(int сhangeableUserId) //n > 0 - Изменение, n < 0 - Создание
+        {
+            InitializeComponent();
+
+            RoleBox.ItemsSource = Core.DB.Roles.Select(r => r.RoleName).ToList();
+
+            if (сhangeableUserId < 0)
+            {
+                isCreated = false;
+                VerifiedBox.IsChecked = true;
+                return;
+            }
+
+            currentUser = Core.DB.Users.Where(s => s.Id == сhangeableUserId).FirstOrDefault();
+
+            if (currentUser == null)
+            {
+                MessageBox.Show("Акак?");
+                return;
+            }
+
+            NameBox.Text = currentUser.Name;
+            SurnameBox.Text = currentUser.Surname;
+            LoginBox.Text = currentUser.Login;
+            PasswordBox.Text = currentUser.Password;
+            RoleBox.SelectedIndex = currentUser.RoleId;
+            VerifiedBox.IsChecked = currentUser.Verified;
+        }
+
+        private void AcceptBTN_Click(object sender, RoutedEventArgs e)
+        {
+            if (isCreated)
+            {
+                Core.DB.Users.Remove(Core.DB.Users.Where(s => s.Id == currentUser.Id).FirstOrDefault());
+                Core.DB.SaveChanges();
+            }
+
+            currentUser = new Users();
+            currentUser.Name = NameBox.Text;
+            currentUser.Surname = SurnameBox.Text;
+            currentUser.Login = LoginBox.Text;
+            currentUser.Password = PasswordBox.Text;
+            currentUser.RoleId = RoleBox.SelectedIndex;
+            currentUser.Verified = Convert.ToBoolean(VerifiedBox.IsChecked);
+
+            Core.DB.Users.Add(currentUser);
+            Core.DB.SaveChanges();
+
+            Core.mainWindow.MainFrame.Navigate(new ControlUsersPage());
+        }
+
+        private void BackBTN_Click(object sender, RoutedEventArgs e)
+        {
+            Core.mainWindow.MainFrame.Navigate(new ControlUsersPage());
+        }
+    }
+}
--- Pages/ControlUsersPage.xaml.cs
+++ Pages/ControlUsersPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DefaultWPF.Resources;
 
 namespace DefaultWPF.Pages
 {
@@ -20,26 +21,62 @@ namespace DefaultWPF.Pages
         public ControlUsersPage()
         {
             InitializeComponent();
+            DG.ItemsSource = Core.DB.Users.ToList();
         }
 
         private void CreateBTN_Click(object sender, RoutedEventArgs e)
         {
-
+            Core.mainWindow.MainFrame.Navigate(new ActionWithUserPage(-1));
         }
 
         private void ChangeBTN_Click(object sender, RoutedEventArgs e)
         {
+            if (DG.SelectedItem == null)
+            {
+                return;
+            }
 
-        }
+            int id = 0;
 
-        private void UpdateBTN_Click(object sender, RoutedEventArgs e)
-        {
+            while (true)
+            {
+                if (DG.SelectedItem == Core.DB.Users.Where(s => s.Id == id).FirstOrDefault())
+                {
+                    break;
+                }
+                id++;
+            }
 
+            Core.mainWindow.MainFrame.Navigate(new ActionWithUserPage(id));
         }
 
         private void BackBTN_Click(object sender, RoutedEventArgs e)
         {
+            Core.mainWindow.MainFrame.Navigate(new AdminPage());
+        }
 
+        private void DeleteBTN_Click(object sender, RoutedEventArgs e)
+        {
+            if (DG.SelectedItem == null)
+            {
+                return;
+            }
+
+            int id = 0;
+
+            while (true)
+            {
+                if (DG.SelectedItem == Core.DB.Users.Where(s => s.Id == id).FirstOrDefault())
+                {
+                    break;
+                }
+                id++;
+            }
+
+            Users deleteUser = Core.DB.Users.Where(s => s.Id == id).FirstOrDefault();
+            Core.DB.Users.Remove(deleteUser);
+            Core.DB.SaveChanges();
+            DG.ItemsSource = Core.DB.Users.ToList();
         }
     }
 }
